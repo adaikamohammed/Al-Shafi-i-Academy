35fab8a74d306216a2b40e8ff51bbd8fbabe21aa
mohammed adaika <mohammed.adaika@univ-bba.dz>
2025-07-25 19:06:25 +0000

    اريد اضافة الشعار بالضبط في مكان شعار الموقع ويكون واضح + اريد كتابة "ال

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -106,7 +106,7 @@ export function Header() {
             <Link href="/" className="flex items-center gap-2">
             <Logo />
             <span className="hidden font-headline text-lg font-bold text-accent sm:inline-block">
-                أكاديمية الشافعي
+                المدرسة القرآنية للإمام الشافعي
             </span>
             </Link>
             {currentDate && (
@@ -158,7 +158,7 @@ export function Header() {
                  <Link href="/" onClick={() => setMobileMenuOpen(false)} className="flex items-center gap-2">
                     <Logo />
                     <span className="font-headline text-lg font-bold text-accent">
-                      أكاديمية الشافعي
+                      المدرسة القرآنية للإمام الشافعي
                     </span>
                  </Link>
               </div>
